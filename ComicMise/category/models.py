cc79a8a739604f991ae30d0f82ba20c8f1d74652
Dhinu C Philip <dhinucphilip1022001@gmail.com>
2024-03-02 23:06:51 +0530

    Product connected with Category using ForeignKey

--- ComicMise/category/models.py
+++ ComicMise/category/models.py
@@ -1,12 +1,22 @@
 from django.db import models
 from django.utils.text import slugify
 
+class YourModelManager(models.Manager):
+    def get_primary_key_by_name(self, name):
+        try:
+            obj = self.get(category_name=name)
+            return obj.pk
+        except Category.DoesNotExist:
+            return None
+
 # Create your models here.
 class Category(models.Model):
     category_name = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(max_length=100, unique=True, blank = True)
     description = models.TextField(max_length=255, blank=True)
     cat_image = models.ImageField(upload_to='static/categories', blank=True)
+
+    objects = YourModelManager()
 
     class Meta:
         verbose_name = 'category'
--- ComicMise/user/views.py
+++ ComicMise/user/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login as auth_login
 from accounts.models import Account
 from category.models import Category
+from store.models import Product
 from django.http import HttpResponse
 from django.contrib import messages
 # Create your views here.
@@ -45,9 +46,9 @@ def adminDashboard(request):
 def categoryView(request):
     if request.method == 'POST':
         cat_name =request.POST['category_name']
-        # cat_slug =request.POST['category_slug']
         cat_description =request.POST['category_description']
         cat_image = request.FILES.get('category_image')
+        
         if not cat_name or not cat_description :# or not cat_image
             messages.error(request,'Enter all fields')
             return redirect ('categoryView')
@@ -61,9 +62,38 @@ def categoryView(request):
         }
     return render(request,'evara-backend/page-categories.html', context)
 
+
 def add_product(request):
-    return render(request,'evara-backend/page-form-product-1.html')
+    if request.method == 'POST':
+        prod_name        = request.POST['product_name']
+        prod_description = request.POST['product_description']
+        prod_price       = request.POST['product_price']
+        prod_image       = request.FILES.get('product_image')
+        prod_category    = request.POST.get('product_category')
+        prod_stock       = request.POST.get('product_stock')
+  
+        # print(prod_name,prod_description,prod_price,prod_category,prod_stock)
+        
+        if not prod_name or not prod_description or not prod_price or not prod_image or not prod_stock or not prod_category:
+            messages.error(request,'Enter the required fields')
+            return redirect('add_product')
+        
+        #getting category using primary key
+        cat_key= Category.objects.get_primary_key_by_name(prod_category)
+        exact_category = Category.objects.get(pk=cat_key)
+        print(prod_name,prod_description,prod_price,prod_stock ,prod_category, cat_key, exact_category)
+        
+        fd = Product(product_name=prod_name, description=prod_description, price=prod_price, stock= prod_stock, category = exact_category, images=prod_image )
+        fd.save()
+        return redirect('add_product')
     
+    category_list = Category.objects.all()
+    context = {
+        'category_list': category_list
+    }
+    return render(request,'evara-backend/page-form-product-1.html', context)
+
+
 def customers_list(request):
     user_set = Account.objects.all()
     return render(request,'evara-backend/page-customers-list.html',{'userlist':user_set})
